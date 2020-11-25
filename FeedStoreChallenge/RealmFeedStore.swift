3b07a18a8eedbe3c7e6d4e6f98141f15d1b3eb8a
Adrimi <adriano100417@gmail.com>
2020-11-25 23:46:08 +0100

    Retrieve command on non-empty cache delivers values
    Add swift flag to include `RealmSwift` framework in `Test` target
    Add `setUp` method to persist Realm data inside in-memory rather on storage

--- FeedStoreChallenge/RealmFeedStore.swift
+++ FeedStoreChallenge/RealmFeedStore.swift
@@ -15,12 +15,59 @@ public class RealmFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add({
+                let realmFeedObject = RealmFeedObject()
+                feed
+                    .map { localFeedImage in
+                        let realmFeedImage = RealmFeedImage()
+                        realmFeedImage.id = localFeedImage.id.uuidString
+                        realmFeedImage.imageDescription = localFeedImage.description
+                        realmFeedImage.location = localFeedImage.location
+                        realmFeedImage.urlString = localFeedImage.url.absoluteString
+                        return realmFeedImage
+                    }
+                    .forEach(realmFeedObject.feedImages.append)
+                realmFeedObject.timestamp = timestamp
+                return realmFeedObject
+            }())
+            completion(nil)
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        let realm = try! Realm()
+        if let localFeedObject = realm
+            .objects(RealmFeedObject.self)
+            .first {
+            completion(.found(feed: localFeedObject.localFeed, timestamp: localFeedObject.timestamp))
+        } else {
+            completion(.empty)
+        }
     }
     
     public init() {}
+}
+
+class RealmFeedObject: Object {
+    var feedImages: List<RealmFeedImage> = .init()
+    @objc dynamic var timestamp: Date = Date()
+    
+    var localFeed: [LocalFeedImage] {
+        feedImages.map {
+            LocalFeedImage(
+                id: UUID(uuidString: $0.id)!,
+                description: $0.imageDescription,
+                location: $0.location,
+                url: URL(string: $0.urlString!)!)
+        }
+    }
+}
+
+class RealmFeedImage: Object {
+    @objc dynamic var id: String = ""
+    @objc dynamic var imageDescription: String? = nil
+    @objc dynamic var location: String? = nil
+    @objc dynamic var urlString: String? = nil
 }
