f40ba3b8971cd4c9bb037205144a05fac838f155
Adrimi <adriano100417@gmail.com>
2020-11-29 12:23:44 +0100

    Improve code readability and maintenance by creating `RealmFeedStoreMapper` helper with extensions to `RealmFeedStore` and `RealmFeedImage`

--- FeedStoreChallenge/RealmFeedStore.swift
+++ FeedStoreChallenge/RealmFeedStore.swift
@@ -17,21 +17,10 @@ public class RealmFeedStore: FeedStore {
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let realm = try! Realm()
         try! realm.write {
-            realm.add({
-                let realmFeedObject = RealmFeedObject()
-                feed
-                    .map { localFeedImage in
-                        let realmFeedImage = RealmFeedImage()
-                        realmFeedImage.id = localFeedImage.id.uuidString
-                        realmFeedImage.imageDescription = localFeedImage.description
-                        realmFeedImage.location = localFeedImage.location
-                        realmFeedImage.urlString = localFeedImage.url.absoluteString
-                        return realmFeedImage
-                    }
-                    .forEach(realmFeedObject.feedImages.append)
-                realmFeedObject.timestamp = timestamp
-                return realmFeedObject
-            }())
+            realm.add(
+                RealmFeedStoreMapper.createRealmFeedObject(
+                    feed: feed,
+                    timestamp: timestamp))
             completion(nil)
         }
     }
@@ -50,24 +39,63 @@ public class RealmFeedStore: FeedStore {
     public init() {}
 }
 
-class RealmFeedObject: Object {
-    var feedImages: List<RealmFeedImage> = .init()
-    @objc dynamic var timestamp: Date = Date()
+class RealmFeedStoreMapper {
+    static func createRealmFeedObject(feed: [LocalFeedImage], timestamp: Date) -> Object {
+        let realmFeedObject = RealmFeedObject()
+        feed.map(RealmFeedStoreMapper.mapToLocal)
+            .forEach(realmFeedObject.feedImages.append)
+        realmFeedObject.timestamp = timestamp
+        return realmFeedObject
+    }
     
-    var localFeed: [LocalFeedImage] {
-        feedImages.map {
-            LocalFeedImage(
-                id: UUID(uuidString: $0.id)!,
-                description: $0.imageDescription,
-                location: $0.location,
-                url: URL(string: $0.urlString!)!)
-        }
+    private static func mapToLocal(_ localFeedImage: LocalFeedImage) -> RealmFeedImage {
+        RealmFeedImage(
+            id: localFeedImage.id.uuidString,
+            imageDescription: localFeedImage.description,
+            location: localFeedImage.location,
+            urlString: localFeedImage.url.absoluteString)
     }
 }
 
+// MARK: - Realm Feed Object
+class RealmFeedObject: Object {
+    var feedImages: List<RealmFeedImage> = .init()
+    @objc dynamic var timestamp: Date = Date()
+}
+
+extension RealmFeedObject {
+    var localFeed: [LocalFeedImage] {
+        feedImages.compactMap(\.localFeed)
+    }
+}
+
+// MARK: - Realm Feed Image
 class RealmFeedImage: Object {
     @objc dynamic var id: String = ""
     @objc dynamic var imageDescription: String? = nil
     @objc dynamic var location: String? = nil
-    @objc dynamic var urlString: String? = nil
+    @objc dynamic var urlString: String = ""
+    
+    override init() {}
+    
+    init(id: String, imageDescription: String?, location: String?, urlString: String) {
+        self.id = id
+        self.imageDescription = imageDescription
+        self.location = location
+        self.urlString = urlString
+    }
+}
+
+extension RealmFeedImage {
+    var localFeed: LocalFeedImage? {
+        guard let id = UUID(uuidString: id),
+              let url = URL(string: urlString) else {
+            return nil
+        }
+        return LocalFeedImage(
+            id: id,
+            description: imageDescription,
+            location: location,
+            url: url)
+    }
 }
